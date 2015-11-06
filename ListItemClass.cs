de6a7c4bbfa446c30d90c9952cefa868b7196d7a
lyceumprog <lyceum.prog@gmail.com>
2015-11-06 11:22:42 +0300

    Update ListItemClass.cs

--- ListItemClass.cs
+++ ListItemClass.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-
+// ШАБЛОН ИНФОРМАЦИИ, КОТОРАЯ БУДЕТ СОДЕРЖАТЬ В СЕБЕ ДАННЫЕ ЯЧЕЙКИ
 namespace CustomListView
 {
 	public class ListItemClass
 	{
-		public string tv1;
-		public string tv2;
-		public int iv;
+		public string tv1; // ДАННЫЕ ПЕРВОГО ПОЛЯ
+		public string tv2; // ДАННЫЕ ВТОРОГО ПОЛЯ
+		public int iv;	   // ДАННЫЕ КАРТИНКИ
 
-		public ListItemClass (string tv1, string tv2, int iv)
+		public ListItemClass (string tv1, string tv2, int iv) // КОНСТРУКТОР
 		{
 			this.tv1 = tv1;
 			this.tv2 = tv2;
